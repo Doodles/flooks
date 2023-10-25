8496c5d2b4319c1ce30d1c04ee19f6a1b8e8301b
javierpozzi <javierpozzi@gmail.com>
2023-10-25 14:24:04 -0300

    fix: fcl changes in useFlowConnect (#4)

--- src/lib/hooks/useFlowConnect.ts
+++ src/lib/hooks/useFlowConnect.ts
@@ -2,8 +2,8 @@ import * as fcl from '@onflow/fcl';
 import { useMutation } from '@tanstack/react-query';
 
 export const useFlowConnect = () => {
-  const data = useMutation({
-    mutationFn: fcl.authenticate,
+  const data = useMutation(async () => {
+    return fcl.authenticate();
   });
 
   return { ...data, connect: data.mutate, connectAsync: data.mutateAsync };
